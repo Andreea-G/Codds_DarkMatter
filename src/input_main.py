bb187e7031ce31ad05e66afbe61613cf26f57fba
Andreea_G <a.georgescu@physics.ucla.edu>
2015-04-14 07:38:52 -0700

    Major change: made Input class

---
+++ src/input_main.py
@@ -0,0 +1,134 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 14 03:40:17 2015
+
+@author: Andreea
+"""
+
+import numpy as np
+import inspect
+from itertools import product
+from runprogram import *
+
+
+input_filename_list = {True: "input_DAMApaper",
+                       False: "input_HaloIndep",
+                       }
+
+
+class Input:
+    def __init__(self, HALO_DEP,
+                 implemented_exper_list=None, index_list=[0], input_slice=slice(None),
+                 scattering_type='SI', scattering_type_list=None,
+                 filename_tail_list=[""], extra_tail="", OUTPUT_MAIN_DIR="../Output/",
+                 MAKE_PLOT=False, RUN_PROGRAM=False, FOX_METHOD=None,
+                 plot_dots=False,
+                 delta_logL=[1, 4]):
+        print(HALO_DEP)
+        print(input_filename_list)
+        module = import_file(input_filename_list[HALO_DEP] + ".py")
+
+        if implemented_exper_list is not None:
+            self.implemented_exper_list = np.array(implemented_exper_list)
+        else:
+            self.implemented_exper_list = \
+                np.array(["superCDMS",
+                          "LUX2013zero", "LUX2013one", "LUX2013three", "LUX2013five", "LUX2013many",
+                          "SIMPLEModeStage2", "PICASSO", "KIMS2012", "DAMA2010NaSmRebinned", "DAMA2010ISmRebinned",
+                          "DAMA2010NaSmRebinned_TotRateLimit",
+                          "XENON10", "XENON100", "CDMSlite2013CoGeNTQ", "CDMSSi2012"])
+        self.SetExperList(index_list)
+        self.scattering_type_list = scattering_type_list \
+            if scattering_type_list is not None \
+            else [scattering_type]
+        self.filename_tail_list = filename_tail_list
+        self.extra_tail = extra_tail  # for Fox method
+        self.input_list = np.array(module.input_list)[input_slice]
+
+        self.OUTPUT_MAIN_DIR = OUTPUT_MAIN_DIR
+        self.MAKE_PLOT = MAKE_PLOT
+        self.plot_dots = plot_dots
+        self.RUN_PROGRAM = RUN_PROGRAM
+        self.HALO_DEP = HALO_DEP
+        self.FOX_METHOD = FOX_METHOD if FOX_METHOD is not None else [False] * 8
+
+        qKIMS_list = [0.05, 0.1]
+        qDAMANa_list = [0.4, 0.3]
+        qDAMAI_list = [0.09, 0.06]
+        qDAMANa_Rate_list = [0.4]
+        self.quenching_list = {"KIMS2012": qKIMS_list,
+                               "DAMA2010NaSmRebinned": qDAMANa_list,
+                               "DAMA2010ISmRebinned": qDAMAI_list,
+                               "DAMA2010NaSmRebinned_TotRateLimit": qDAMANa_Rate_list,
+                               }
+
+        self.fp = 1
+
+        self.delta_logL = delta_logL  # for Fox method
+
+    def SetExperList(self, index_list):
+        self.exper_list = self.implemented_exper_list[index_list]
+
+    def SetScattering_type(self, scattering_type_list):
+        try:
+            len(scattering_type_list)
+        except TypeError:
+            self.scattering_type_list = [scattering_type_list]
+        else:
+            self.scattering_type_list = scattering_type_list
+
+    def SetInputList(self, input_slice):
+        module = import_file(input_filename_list[self.HALO_DEP] + ".py")
+        self.input_list = np.array(module.input_list)[input_slice]
+
+    def _GetKwargs(self):
+        attributes = inspect.getmembers(self, lambda a: not(inspect.isroutine(a)))
+        kwargs = dict([a for a in attributes
+                       if '__' not in a[0] and '_list' not in a[0]])
+#        print(kwargs)
+        return kwargs
+
+    def Run_HaloIndep(self):
+        module = import_file(input_filename_list[self.HALO_DEP] + ".py")
+        for self.exper_name, self.scattering_type, self.filename_tail, \
+                (self.mx, self.fn, self.delta, self.mPhi) \
+                in product(self.exper_list, self.scattering_type_list,
+                           self.filename_tail_list, self.input_list):
+            for self.quenching in self.quenching_list.get(self.exper_name, [None]):
+                self.vmin_range = \
+                    module.Vmin_range(self.exper_name, self.mx, self.delta, mPhi=self.mPhi,
+                                      quenching=self.quenching,
+                                      FOX_METHOD=np.any(self.FOX_METHOD))
+                self.vmin_FoxBand_range = \
+                    module.Vmin_FoxBand_range(self.exper_name, self.mx, self.delta, self.mPhi)
+                print(self.vmin_range)
+                kwargs = self._GetKwargs()
+                run_program(**kwargs)
+        return
+
+    def Run_HaloDep(self):
+        module = import_file(input_filename_list[self.HALO_DEP] + ".py")
+        for self.exper_name, self.scattering_type, self.filename_tail, \
+                (self.fn, self.delta, self.mPhi) \
+                in product(self.exper_list, self.scattering_type_list,
+                           self.filename_tail_list, self.input_list):
+            for self.quenching in self.quenching_list.get(self.exper_name, [None]):
+                self.mx_range = \
+                    module.DM_mass_range(self.exper_name, self.delta, self.mPhi,
+                                         self.quenching)
+                print(self.mx_range)
+                kwargs = self._GetKwargs()
+                run_program(**kwargs)
+        return
+
+    def RunProgram(self):
+        if self.HALO_DEP:
+            self.Run_HaloDep()
+        else:
+            self.Run_HaloIndep()
+        return
+
+
+if __name__ == "__main__":
+    inp = Input()
+    inp.RunProgram()
--- src/main.py
+++ src/main.py
@@ -4,115 +4,52 @@ Created on Thu Nov 20 22:52:11 2014
 
 @author: Andreea
 """
-from __future__ import print_function
-from __future__ import division
 import profile
-from runprogram import *
 import os   # for speaking
-#from input_DAMApaper import *
-from input_HaloIndep import *
+from input_main import *
 
 
 def main():
-    implemented_exper = ["superCDMS",
-                         "LUX2013zero", "LUX2013one", "LUX2013three", "LUX2013five", "LUX2013many",
-                         "SIMPLEModeStage2", "PICASSO", "KIMS2012", "DAMA2010NaSmRebinned", "DAMA2010ISmRebinned",
-                         "DAMA2010NaSmRebinned_TotRateLimit",
-                         "XENON10", "XENON100", "CDMSlite2013CoGeNTQ", "CDMSSi2012"]
-    scattering_type = 'SI'
-    fp = 1.
+    implemented_exper_list = \
+        ["superCDMS",
+         "LUX2013zero", "LUX2013one", "LUX2013three", "LUX2013five", "LUX2013many",
+         "SIMPLEModeStage2", "PICASSO", "KIMS2012", "DAMA2010NaSmRebinned", "DAMA2010ISmRebinned",
+         "DAMA2010NaSmRebinned_TotRateLimit",
+         "XENON10", "XENON100", "CDMSlite2013CoGeNTQ", "CDMSSi2012"]
+#    inp.FOX_METHOD = [F] * 8
+#    inp.FOX_METHOD = [T, T, T, F, F, F, F, F]     # Multiple
+#    inp.FOX_METHOD = [T, F, F, F, F, F, F, F]     # ResponseTables
+#    inp.FOX_METHOD = [F, T, F, F, F, F, F, F]     # OptimalLikelihood
+#    inp.FOX_METHOD = [F, F, T, F, F, F, F, F]     # ImportOptimalLikelihood
+#    inp.FOX_METHOD = [F, F, F, T, F, F, F, F]     # ConstrainedOptimalLikelihood
+#    inp.FOX_METHOD = [F, F, F, F, T, F, F, F]     # VminLogetaSamplingTable
+#    inp.FOX_METHOD = [F, F, F, F, F, T, F, F]     # LogLikelihoodList
+#    inp.FOX_METHOD = [F, F, F, F, F, F, T, F]     # FoxBand
+#    inp.FOX_METHOD = [F, F, F, F, T, F, F, T]     # FoxBandPlot
+    HALO_DEP = T
+    plot_dots = T
+    RUN_PROGRAM = F
+    MAKE_PLOT = T
 
-    plot_dots = F
-    RUN_PROGRAM = T
-    MAKE_PLOT = F
-    HALO_DEP = F
-    HALO_INDEP = not HALO_DEP
-    FOX_METHOD = [F] * 8
-#    FOX_METHOD = [T, T, T, F, F, F, F, F]     # Multiple
-#    FOX_METHOD = [T, F, F, F, F, F, F, F]     # ResponseTables
-#    FOX_METHOD = [F, T, F, F, F, F, F, F]     # OptimalLikelihood
-#    FOX_METHOD = [F, F, T, F, F, F, F, F]     # ImportOptimalLikelihood
-#    FOX_METHOD = [F, F, F, T, F, F, F, F]     # ConstrainedOptimalLikelihood
-#    FOX_METHOD = [F, F, F, F, T, F, F, F]     # VminLogetaSamplingTable
-#    FOX_METHOD = [F, F, F, F, F, T, F, F]     # LogLikelihoodList
-#    FOX_METHOD = [F, F, F, F, F, F, T, F]     # FoxBand
-#    FOX_METHOD = [F, F, F, F, T, F, F, T]     # FoxBandPlot
+    inp = Input(HALO_DEP, implemented_exper_list=implemented_exper_list,
+                RUN_PROGRAM=RUN_PROGRAM, MAKE_PLOT=MAKE_PLOT, plot_dots=plot_dots)
 
-    qKIMS_list = [0.05, 0.1]
-    qDAMANa_list = [0.4, 0.3]
-    qDAMAI_list = [0.09, 0.06]
-    qDAMANa_Rate_list = [0.4]
-    quenching_list = {"KIMS2012": qKIMS_list,
-                      "DAMA2010NaSmRebinned": qDAMANa_list,
-                      "DAMA2010ISmRebinned": qDAMAI_list,
-                      "DAMA2010NaSmRebinned_TotRateLimit": qDAMANa_Rate_list,
-                      }
-
-    exper_list = [implemented_exper[i] for i in [0]]
-#    exper_list = implemented_exper
-    filename_tail_list = [""]
-    OUTPUT_MAIN_DIR = "../Output1/"
-
-#    extra_tail = "_junk"
-#    vmin_index_list = np.array([1])
-#    logeta_index_range = np.array([3, 4])
-
-    extra_tail = "_mix"
-    vmin_index_list = None
-#    vmin_index_list = 13
-    logeta_index_range = None
-    delta_logL = [1, 4]
+    inp.SetScattering_type(['SI', 'SDAV', 'SDPS'])
+    inp.SetExperList([0])
+    inp.SetInputList(slice(0, 1))
+    inp.OUTPUT_MAIN_DIR = "../Output1/"
+    inp.filename_tail_list = ["_delete"]
+    inp.extra_tail = "_mix"
 
     try:
         plt.close()
-
-        if HALO_INDEP:
-            for exper_name in exper_list:
-                for filename_tail in filename_tail_list:
-                    for (mx, fn, delta, mPhi) in input_list[0:1]:
-                        for quenching in quenching_list.get(exper_name, [None]):
-                            (vmin_start, vmin_end, vmin_step) = \
-                                Vmin_range(exper_name, mx, delta, mPhi=mPhi,
-                                           quenching=quenching,
-                                           FOX_METHOD=np.any(FOX_METHOD))
-                            vmin_FoxBand_range = \
-                                Vmin_FoxBand_range(exper_name, mx, delta, mPhi)
-                            print(vmin_start, " ", vmin_end, " ", vmin_step)
-                            run_program(exper_name, scattering_type, mPhi, fp, fn, delta,
-                                        RUN_PROGRAM, MAKE_PLOT, HALO_DEP, FOX_METHOD,
-                                        mx=mx, vmin_range=(vmin_start, vmin_end, vmin_step),
-                                        vmin_FoxBand_range=vmin_FoxBand_range,
-                                        logeta_FoxBand_percent_range=logeta_FoxBand_percent_range,
-                                        steepness=Steepness(exper_name, mx, delta, mPhi),
-                                        logeta_guess=Logeta_guess(exper_name, mx, delta, mPhi),
-                                        vmin_index_list=vmin_index_list,
-                                        logeta_index_range=logeta_index_range,
-                                        delta_logL=delta_logL,
-                                        filename_tail=filename_tail, extra_tail=extra_tail,
-                                        OUTPUT_MAIN_DIR=OUTPUT_MAIN_DIR,
-                                        plot_dots=plot_dots, quenching=quenching)
-
-        if HALO_DEP:
-            for exper_name in exper_list:
-                for filename_tail in filename_tail_list:
-                    for (fn, delta, mPhi) in input_list[0:1]:
-                        for quenching in quenching_list.get(exper_name, [None]):
-                            (mx_min, mx_max, num_steps) = \
-                                DM_mass_range(exper_name, delta, mPhi, quenching)
-                            print(mx_min, " ", mx_max, " ", num_steps)
-                            run_program(exper_name, scattering_type, mPhi, fp, fn, delta,
-                                        RUN_PROGRAM, MAKE_PLOT, HALO_DEP, FOX_METHOD,
-                                        mx_range=(mx_min, mx_max, num_steps),
-                                        filename_tail=filename_tail,
-                                        OUTPUT_MAIN_DIR=OUTPUT_MAIN_DIR,
-                                        plot_dots=plot_dots, quenching=quenching)
-
+        inp.RunProgram()
         plt.show()
-
     finally:
-        if RUN_PROGRAM:
-            None
-#            os.system("say 'Finished running program'")
+        if inp.RUN_PROGRAM:
+            os.system("say 'Finished running program'")
+#            None
+
 
 if __name__ == '__main__':
     main()
