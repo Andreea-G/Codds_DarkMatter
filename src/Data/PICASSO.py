9f4ed9f16da73c20aa27f041d33a439b620ee323
Andreea_G <a.georgescu@physics.ucla.edu>
2015-05-28 00:45:48 -0700

    Fixed bug in Picasso data

--- src/Data/PICASSO.py
+++ src/Data/PICASSO.py
@@ -64,9 +64,9 @@ BinError = np.array([7.170051, 1.77665, 9.073604, 9.200507, 1.269036,
                      1.649746, 1.77665, 4.63198])
 
 if ConfidenceLevel == 0.9:
-    chiSquared = 13.3616
+    chiSquared = {1: 2.70554, 8: 13.3616}
 elif ConfidenceLevel == 0.99:
-    chiSquared = 20.0902
+    chiSquared = {1: 6.6349, 8: 20.0902}
 else:
-    chiSquared = 13.3616
+    chiSquared = {1: 2.70554, 8: 13.3616}
     print("Warning! You asked for a ConfidenceLevel that is not given! Using 0.9 instead.")
