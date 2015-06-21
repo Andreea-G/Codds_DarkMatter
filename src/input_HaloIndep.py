a535afc5278ba27223dd947d23c4d9f99017c01c
Andreea_G <a.georgescu@physics.ucla.edu>
2015-06-21 16:06:14 -0700

    Added default for Vmin_range in halo independent input

--- src/input_HaloIndep.py
+++ src/input_HaloIndep.py
@@ -11,33 +11,19 @@ def Vmin_range(exper_name, mx, delta, mPhi=1000., quenching=None, FOX_METHOD=Fal
         vmin_step = 0.5
         vmin_range_options = {(9., 0, 1000.): (vmin_step, 2000, vmin_step),
                               (3.5, -50, 1000.): (vmin_step, 2000, vmin_step),
-                              (1.3, -200, 1000.): (vmin_step, 2000, vmin_step),
-                              (33., 0, 1000.): (vmin_step, 1000, vmin_step),
-                              (47., 0, 1000.): (vmin_step, 1000, vmin_step),
-                              (38, 0, 50, 1000.): (vmin_step, 1000, vmin_step),
-                              (58, 50, 1000.): (vmin_step, 1000, vmin_step)
+                              (1.3, -200, 1000.): (vmin_step, 2000, vmin_step)
                               }
     elif "LUX" in exper_name:
         vmin_step = 5
         vmin_range_options = {(9., 0, 1000.): (450, 1000, vmin_step),
                               (3.5, -50, 1000.): (550, 1000, vmin_step),
-                              (1.3, -200, 1000.): (750, 1000, vmin_step),
-                              (33., 0, 1000.): (vmin_step, 1000, vmin_step),
-                              (47., 0, 1000.): (vmin_step, 1000, vmin_step),
-                              (38, 50, 1000.): (vmin_step, 1000, vmin_step),
-                              (58, 50, 1000.): (vmin_step, 1000, vmin_step)
+                              (1.3, -200, 1000.): (750, 1000, vmin_step)
                               }
     else:
         vmin_step = 5
-        vmin_range_options = {(9., 0, 1000.): (vmin_step, 1000, vmin_step),
-                              (3.5, -50, 1000.): (vmin_step, 1000, vmin_step),
-                              (1.3, -200, 1000.): (vmin_step, 1000, vmin_step),
-                              (33., 0, 1000.): (vmin_step, 1000, vmin_step),
-                              (47., 0, 1000.): (vmin_step, 1000, vmin_step),
-                              (38, 50, 1000.): (vmin_step, 1000, vmin_step),
-                              (58, 50, 1000.): (vmin_step, 1000, vmin_step)
-                              }
-    return vmin_range_options[(mx, delta, mPhi)]
+        vmin_range_options = {}
+    default = (vmin_step, 1000, vmin_step)
+    return vmin_range_options.get((mx, delta, mPhi), default)
 
 
 def Steepness(exper_name, mx, delta, mPhi=1000.):
@@ -77,8 +63,10 @@ def Vmin_FoxBand_range(exper_name, mx, delta, mPhi=1000.):
 
 
 # input of the form (mx, fn, delta, mPhi)
-input_list = [(9., -0.8, 0., 1000.), (3.5, -0.8, -50, 1000.), (1.3, -0.8, -200, 1000),
-              (9., 1, 0, 1000.), (9., -0.7, 0, 1000.), (9, 0, 0, 1000),
-              (3.5, 1, -50, 1000.), (33., 0, 0, 1000), (47., 0, 0, 1000),
-              (38, 0, 50, 1000.), (58, 0, 50, 1000)]
+input_list = [(9., -0.8, 0., 1000.), (3.5, -0.8, -50, 1000.), (1.3, -0.8, -200, 1000),  # 0 - 2
+              (9., 1, 0, 1000.), (9., -0.7, 0, 1000.), (9, 0, 0, 1000),  # 3 - 5
+              (3.5, 1, -50, 1000.), (33., 0, 0, 1000), (47., 0, 0, 1000),  # 6 - 8
+              (7, 0, 0, 1000), (34.541, 0, 0, 1000), (57.35, 0, 0, 1000),  # 9 - 11, SDPS
+              (38, 0, 50, 1000.), (45, 0, 100, 1000),  # 12 - 13, SDPS
+              (58, 0, 50, 1000), (52, 0, 100, 1000), (80, 0, 100, 0)]  # 14 - 16, SDAV
 logeta_FoxBand_percent_range = (0.2, 0.2, 50)
