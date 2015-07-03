5c41ba646ef35fa3a5d7993d1b419765a2968706
Andreea_G <a.georgescu@physics.ucla.edu>
2015-07-03 01:25:04 -0700

    Adjusted parameters in input_HaloDep

--- src/input_HaloDep.py
+++ src/input_HaloDep.py
@@ -57,51 +57,55 @@ def DM_mass_range(exper_name, delta, mPhi=1000., quenching=None):
         if quenching == 0.4:
             mx_range_options = {(0, 1000.): (5, 25, num_steps),
 #            mx_range_options = {(0, 1000.): (8, 15, num_steps),
-                                (0, 0.): (6, 22, num_steps),
+                                (0, 0.): (5.5, 25, num_steps),
                                 (-30, 1000.): (2, 4, num_steps),
                                 (-30, 0.): (2, 3, num_steps),
-                                (-50, 1000.): (1.5, 2.5, num_steps),
+                                (-50, 1000.): (1.4, 2.5, num_steps),
                                 }
         else:
             mx_range_options = {(0, 1000.): (6, 30, num_steps),
-#            mx_range_options = {(0, 1000.): (10, 20, num_steps),
-                                (0, 0.): (7, 22, num_steps),
+#            mx_range_options = {(0, 1000.): (10, 25, num_steps),
+#                                (0, 0.): (7, 22, num_steps),
+                                (0, 0.): (7, 29, num_steps),
                                 (-30, 1000.): (3, 5, num_steps),
                                 (-30, 0.): (2.5, 4, num_steps),
-                                (-50, 1000.): (2, 3.5, num_steps),
+                                (-50, 1000.): (1.9, 3.5, num_steps),
                                 }
     elif exper_name == "DAMA2010ISmRebinned":
-        num_steps = 60
-        if quenching == 0.09:
-            mx_range_options = {(0, 1000.): (30, 80, num_steps),
-#            mx_range_options = {(0, 1000.): (40, 65, num_steps),
-                                (0, 0.): (22, 90, num_steps),
-                                (-30, 1000.): (20, 80, num_steps),
-                                (-30, 0.): (30, 100, num_steps),
-                                (-50, 1000.): (20, 40, num_steps),
-                                (50, 1000.): (30, 100, num_steps),
-                                (50, 0.): (40, 100, num_steps),
-                                (100, 1000.): (42, 80, num_steps),
-                                (100, 0.): (30, 300, num_steps),
-#                                (100, 0.): (42, 65, num_steps),
-                                }
-        else:
+        num_steps = 80
+        if quenching == 0.06:
             mx_range_options = {(0, 1000.): (30, 100, num_steps),
 #            mx_range_options = {(0, 1000.): (55, 85, num_steps),
-                                (0, 0.): (22, 130, num_steps),
-                                (-30, 1000.): (30, 100, num_steps),
+#                                (0, 0.): (22, 130, num_steps),
+                                (0, 0.): (22, 100, num_steps),
+                                (-30, 1000.): (25, 100, num_steps),
                                 (-30, 0.): (40, 100, num_steps),
-                                (-50, 1000.): (30, 50, num_steps),
-                                (50, 1000.): (40, 100, num_steps),
+                                (-50, 1000.): (25, 50, num_steps),
+                                (50, 1000.): (35, 100, num_steps),
                                 (50, 0.): (55, 100, num_steps),
-                                (100, 1000.): (50, 100, num_steps),
+                                (100, 1000.): (45, 100, num_steps),
                                 (100, 0.): (50, 500, num_steps),
 #                                (100, 0.): (50, 90, num_steps),
                                 }
+        else:
+            mx_range_options = {(0, 1000.): (22, 80, num_steps),
+#            mx_range_options = {(0, 1000.): (40, 65, num_steps),
+#                                (0, 0.): (29, 90, num_steps),
+                                (0, 0.): (22, 100, num_steps),
+                                (-30, 1000.): (20, 80, num_steps),
+                                (-30, 0.): (30, 100, num_steps),
+                                (-50, 1000.): (18, 40, num_steps),
+                                (50, 1000.): (30, 100, num_steps),
+                                (50, 0.): (40, 100, num_steps),
+                                (100, 1000.): (41, 100, num_steps),
+                                (100, 0.): (30, 300, num_steps),
+#                                (100, 0.): (42, 65, num_steps),
+                                }
     elif exper_name == "DAMA2010NaSmRebinned_TotRateLimit":
         num_steps = 60
+#        mx_range_options = {(0, 1000.): (3, 15, num_steps),
         mx_range_options = {(0, 1000.): (3, 20, num_steps),
-                            (0, 0.): (3, 20, num_steps),
+                            (0, 0.): (3, 15, num_steps),
                             (-30, 1000.): (1, 10, num_steps),
                             (-30, 0.): (1, 10, num_steps),
                             (-50, 1000.): (1, 10, num_steps),
@@ -119,6 +123,6 @@ def DM_mass_range(exper_name, delta, mPhi=1000., quenching=None):
     return mx_range_options[(delta, mPhi)]
 
 
-input_list = [(-1/16.4, 0, 1000.),
-              (0, 0, 1000.), (0, -30, 1000.),  (0, -50, 1000.), (0, 50, 1000.), (0, 100, 1000.),
-              (0, 0, 0.), (0, -30, 0.), (0, 100, 0.)]
+input_list = [(-1/16.4, 0, 1000.),  # 0
+              (0, 0, 1000.), (0, -30, 1000.),  (0, -50, 1000.), (0, 0, 0.), (0, -30, 0.),  # 1 - 5
+              (0, 50, 1000.), (0, 100, 1000.), (0, 100, 0.)]  # 6 - 8
