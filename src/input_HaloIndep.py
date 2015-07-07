040997d460a2a18382d91ad11fe34d8ea5cbd719
Andreea_G <a.georgescu@physics.ucla.edu>
2015-07-06 17:03:03 -0700

    adjusted input for halo-indep

--- src/input_HaloIndep.py
+++ src/input_HaloIndep.py
@@ -4,25 +4,45 @@ Created on Sun Mar  1 21:28:44 2015
 
 @author: Andreea
 """
+from globalfnc import VMin
 
 
 def Vmin_range(exper_name, mx, delta, mPhi=1000., quenching=None, FOX_METHOD=False):
+    vmin_range_options = {}
     if exper_name == "CDMSSi2012" and FOX_METHOD:
-        vmin_step = 0.5
-        vmin_range_options = {(9., 0, 1000.): (vmin_step, 2000, vmin_step),
-                              (3.5, -50, 1000.): (vmin_step, 2000, vmin_step),
-                              (1.3, -200, 1000.): (vmin_step, 2000, vmin_step)
-                              }
+        vmin_step = vmin_min = 0.5
+        vmin_max = 2000
     elif "LUX" in exper_name:
-        vmin_step = 5
-        vmin_range_options = {(9., 0, 1000.): (450, 1000, vmin_step),
-                              (3.5, -50, 1000.): (550, 1000, vmin_step),
-                              (1.3, -200, 1000.): (750, 1000, vmin_step)
+        vmin_step = vmin_min = 5
+        vmin_max = 800
+        vmin_range_options = {(9., 0, 1000.): (450, vmin_max, vmin_step),
+                              (3.5, -50, 1000.): (550, vmin_max, vmin_step),
+                              (1.3, -200, 1000.): (750, vmin_max, vmin_step)
                               }
+    elif "DAMA" in exper_name:
+        vmin_step = vmin_min = 1
+        if "Na" in exper_name:
+            mT = 21.4148
+            qT = 0.3
+        else:
+            mT = 118.211
+            qT = 0.06
+        Emax = 20
+        vmin_max = round(VMin(Emax/qT, mT, mx, delta)) + 200
+        if delta == 0:
+            Ethreshold = 2.
+            qT = 0.09
+            vmin_step = VMin(Ethreshold/qT, mT, mx, delta)
+        vmin_range_options = {(30.14, 0, 1000.): (vmin_step, vmin_max, 1000),
+                              (47.35, 0, 1000.): (vmin_step, vmin_max, 600)
+                              }
+    elif "KIMS" in exper_name:
+        vmin_min = vmin_step = 1
+        vmin_max = 1000
     else:
-        vmin_step = 5
-        vmin_range_options = {}
-    default = (vmin_step, 1000, vmin_step)
+        vmin_step = vmin_min = 1
+        vmin_max = 1000
+    default = (vmin_min, vmin_max, vmin_step)
     return vmin_range_options.get((mx, delta, mPhi), default)
 
 
@@ -66,8 +86,7 @@ def Vmin_FoxBand_range(exper_name, mx, delta, mPhi=1000.):
 input_list = [(9., -0.8, 0., 1000.), (3.5, -0.8, -50, 1000.), (1.3, -0.8, -200, 1000),  # 0 - 2
               (9., 1, 0, 1000.), (9., -0.7, 0, 1000.), (9, 0, 0, 1000),  # 3 - 5
               (3.5, 1, -50, 1000.), (33., 0, 0, 1000), (47., 0, 0, 1000),  # 6 - 8
-              (7, 0, 0, 1000), (30.14, 0, 0, 1000), (57.35, 0, 0, 1000),  # 9 - 11, SDPS
+              (7, 0, 0, 1000), (30.14, 0, 0, 1000), (47.35, 0, 0, 1000),  # 9 - 11, SDPS
               (38, 0, 50, 1000.), (45, 0, 100, 1000),  # 12 - 13, SDPS
-              (40, 0, 50, 1000), (52, 0, 100, 1000), (80, 0, 100, 0),  # 14 - 16, SDAV
-              (34.541, 0, 0, 1000)]  # 17, SDPS
+              (40, 0, 50, 1000), (52, 0, 100, 1000), (80, 0, 100, 0)]  # 14 - 16, SDAV
 logeta_FoxBand_percent_range = (0.2, 0.2, 50)
