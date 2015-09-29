3cdfea69c50567bd631b1ebdafa5ad0e32224ff7
Samuel Witte <witte.sam@gmail.com>
2015-09-29 10:08:13 -0700

    Altered commented sections in lambertw.py and changed variable and function name.

--- src/lambertw.py
+++ src/lambertw.py
@@ -1,5 +1,6 @@
 """
 Copyright (c) 2015 Andreea Georgescu
+Samuel Witte
 
 Created on Monday Sep 21 2015
 
@@ -16,38 +17,15 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-Code Taken from http://blogs.mathworks.com/cleve/2013/09/02/the-lambert-w-function/
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-    Redistributions of source code must retain the above copyright notice,
-    this list of conditions and the following disclaimer.
-    Redistributions in binary form must reproduce the above copyright notice,
-    this list of conditions and the following disclaimer in the documentation
-    and/or other materials provided with the distribution.
-    Neither the name of Enthought nor the names of the SciPy Developers may
-    be used to endorse or promote products derived from this software without
-    specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS”
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY
-DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+Code taken from http://blogs.mathworks.com/cleve/2013/09/02/the-lambert-w-function/ 
 
 
-In case lambertw.py is not correctly installed this can be used.
+In case scipy.special.lambertw.py is not correctly installed this can be used.
 """
 import math
 
 
-def w0Lambert(x, k):
+def lambertw(x, k):
     eps = 0.00000001  # max error allowed
 
     if k != -1:
@@ -58,8 +36,8 @@ def w0Lambert(x, k):
     while True:
         ew = math.exp(w)
         f = w * ew - x
-        wNew = w - f/((ew * (w + 1.) - (w + 2.) * f/(2. * w + 2.)))
-        if abs(w - wNew) <= eps:
+        w_new = w - f/((ew * (w + 1.) - (w + 2.) * f/(2. * w + 2.)))
+        if abs(w - w_new) <= eps:
             break
-        w = wNew
+        w = w_new
     return w
